d20457fa08ad7465452f4a60aa21a34466d084f2
zhaohuajing <huajing_zhao@berkely.edu>
2026-02-24 13:41:47 -0500

    Updated grasp_executor_rgbd_server with base_path

--- contact_graspnet_ros2/grasp_executor_rgbd_server.py
+++ contact_graspnet_ros2/grasp_executor_rgbd_server.py
@@ -24,7 +24,7 @@ class GraspServer(Node):
         self.get_logger().info('Grasp server ready (executing inference inside a docker container).')
 
         # Base path inside the docker container and host
-        self.base_path = "/home/csrobot/graspnet_ws/src/contact_graspnet_ros2/contact_graspnet"
+        self.base_path = os.path.expanduser('~/graspnet_ws/src/contact_graspnet_ros2/contact_graspnet')
 
         # Whether to parse JSON from stdout or load the .npz file directly
         self.result_loading = "_use_json"  # ["_use_json", "_use_npz"]
